4f2576e704c85ba624853ab9a8fc28eae82ac33d
maabcodingschool <82595824+maabcodingschool@users.noreply.github.com>
2021-05-05 18:46:46 +0500

    Add files via upload

---
+++ Udemy(Day{8-10})/Day9/app.js
@@ -0,0 +1,38 @@
+const sounds = ['Lalipop','Luna','NashleT','Moruzya','Ya_Naydu','Ti_Ne']
+
+sounds.forEach(sound=>{
+    const btn=document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', ()=>{
+        stopSong()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSong(){
+    sounds.forEach(sound=>{
+        const song=document.getElementById(sound)
+
+        song.pause()
+        song.currentTime=0;
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
